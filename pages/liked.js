6d232de3f9081301efc8a94337ae5836fe27c5fc
AndrewSova <asovacloud@gmail.com>
2020-11-26 01:18:09 +0200

    Add Liked Page

--- pages/liked.js
+++ pages/liked.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { ReactReduxContext } from 'react-redux';
 
 const Liked = () => {
     return (
-        <Layout>
-            <main id="main">
-                Liked Content
-            </main>
-        </Layout>
+        <ReactReduxContext.Consumer>
+            {
+                (store) => {
+                    const likedPosts = store.store.getState().likePosts;
+                    return (
+                        <Layout>
+                            <main id="main">
+                                {
+                                    !likedPosts.length ? (
+                                        <h5>You haven't liked any posts yet. Please, like posts.</h5>
+                                    ) : (
+                                        <>
+                                            <h5>This posts are interesting for you.</h5>
+                                            <ul className="collection">
+                                                {
+                                                    likedPosts.map((post, idx) => <li key={idx} className="collection-item">{post}</li>)
+                                                }
+                                            </ul>
+                                        </>
+                                    )
+                                }
+                            </main>
+                        </Layout>
+                    );
+                }
+            }
+        </ReactReduxContext.Consumer>
     );
 };
 
